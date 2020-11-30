968cd368f12143cf3b591d3bb9755f8e6150bd9b
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-30 06:14:50 -0800

    Update box3.js

--- box3.js
+++ box3.js
@@ -10,23 +10,20 @@ class Box3 {
     }
     display(){
         var position = this.body.position;
-        rectMode(CENTER);
-        fill("#b7ff00");
-        rect(position.x, position.y, this.width, this.height);
         if(this.body.speed<3){
             rectMode(CENTER);
-            rect();
+            fill("#b7ff00");
+            rect(position.x, position.y, this.width, this.height);
         }
         else{
             World.remove(world, this.body);
             push();
             this.visibility = this.visibility-5;
-            tint(255, this.visibility);
             pop();
         }
     }
     score(){
-        if(this.visibility<0&&this.visibility>=-100){
+        if(this.visibility<0&&this.visibility>=-105){
             score++;
         }
     }
