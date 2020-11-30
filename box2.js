c2572f6e997ca6d06e99335998cba90a79c20222
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-30 06:15:35 -0800

    Update box2.js

--- box2.js
+++ box2.js
@@ -10,23 +10,20 @@ class Box2 {
     }
     display(){
         var position = this.body.position;
-        rectMode(CENTER);
-        fill("#42ffa1");
-        rect(position.x, position.y, this.width, this.height);
         if(this.body.speed<3){
-           rectMode(CENTER);
-           rect();
+            fill("#42ffa1");
+            rectMode(CENTER);
+           rect(position.x, position.y, this.width, this.height);
         }
         else{
             World.remove(world, this.body);
             push();
             this.visibility = this.visibility-5;
-            tint(255, this.visibility);
             pop();
         }
     }
     score(){
-        if(this.visibility<0&&this.visibility>=-100){
+        if(this.visibility<0&&this.visibility>=-105){
             score++;
         }
     }
