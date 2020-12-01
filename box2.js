b01c473593d668efe4b896dfcf58a9529d8f8afb
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-01 06:45:19 -0800

    Update box2.js

--- box2.js
+++ box2.js
@@ -10,7 +10,8 @@ class Box2 {
     }
     display(){
         var position = this.body.position;
-        if(this.body.speed<3){
+        this.visibility = 255;
+        if(this.body.speed<9){
             fill("#42ffa1");
             rectMode(CENTER);
            rect(position.x, position.y, this.width, this.height);
