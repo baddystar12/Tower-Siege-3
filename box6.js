3b2388f276f919861c937e1c489cfa29dfaccad9
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-12-02 06:50:52 -0800

    Update box6.js

--- box6.js
+++ box6.js
@@ -6,7 +6,7 @@ class Box6 {
       this.body = Bodies.rectangle(x,y,width,height,box6_options);
       this.width = width;
       this.height = height;
-      this.visiblity = 255;
+      this.visibility = 255;
       World.add(world, this.body);
     }
     display(){
